24b4223e2a73d89d9b04917c055c0958b8808f59
reyhaneh <forrgh76@gmail.com>
2025-02-10 13:37:40 +0330

    work on new template files (part1)
    + base.html
    edit about | contact | index .html files

--- RGHwebsite/urls.py
+++ RGHwebsite/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from RGHwebsite.views import *
 
 urlpatterns = [
-    path('about/', about),
-    path('contact/', contact),
-    path('', home)
+    path('about/', about, name = 'about'),
+    path('contact/', contact, name='contact'),
+    path('', home, name='index')
 
 
 ]
